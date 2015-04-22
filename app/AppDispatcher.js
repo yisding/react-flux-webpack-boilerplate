d3ccf5f45556d09b7094ead405e217439515485b
Yi Ding <yi@agoradays.com>
2015-04-22 12:31:33 -0700

    trying another way to get the appdispatcher to work

--- app/AppDispatcher.js
+++ app/AppDispatcher.js
@@ -1,20 +1,18 @@
 var Dispatcher = require('flux/lib/Dispatcher');
-var assign = require('object-assign');
 
-var AppDispatcher = assign(new Dispatcher, {
+var AppDispatcher = new Dispatcher();
 
-  /**
-   * A bridge function between the views and the dispatcher, marking the action
-   * as a view action.  Another variant here could be handleServerAction.
-   * @param  {object} action The data coming from the view.
-   */
-  handleViewAction: function(action) {
-    this.dispatch({
-      source: 'VIEW_ACTION',
-      action: action
-    });
-  }
 
-});
+/**
+ * A bridge function between the views and the dispatcher, marking the action
+ * as a view action.  Another variant here could be handleServerAction.
+ * @param  {object} action The data coming from the view.
+ */
+AppDispatcher.handleViewAction = function(action) {
+  this.dispatch({
+    source: 'VIEW_ACTION',
+    action: action
+  });
+}
 
 module.exports = AppDispatcher;
